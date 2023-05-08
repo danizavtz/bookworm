91eb35e33c4eb13b131992a49b179ad634ac61d3
danizavtz <daniellucena@yahoo.com.br>
2023-05-08 19:40:14 -0300

    Initial Commit

---
+++ Shared/bookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookwormApp.swift
+//  Shared
+//
+//  Created by Daniel Lucena Pires on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct bookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
